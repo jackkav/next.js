c18112ad9ddd6a2142b48095b10b247da0ecb932
Jack Kavanagh <jackkav@gmail.com>
2019-02-13 00:42:14 +0100

    Testing with next storybook

--- examples/with-lingui/pages/index.js
+++ examples/with-lingui/pages/index.js
@@ -2,13 +2,16 @@ import Link from 'next/link'
 import { Trans } from '@lingui/macro'
 import withLang from '../components/withLang'
 import LangSwitcher from '../components/LangSwitcher'
-
-const Index = () =>
-  <div>
+import styled from '@emotion/styled/macro'
+const Red = styled.div`
+color: red;
+`
+const Index = () =>console.log('test')||
+  <Red>
     <Trans>Hello World.</Trans>
     <Link href='/two'><a><Trans>Go to page 2</Trans></a></Link>
     <br />
     <LangSwitcher />
-  </div>
+  </Red>
 
 export default withLang(Index)
---
+++ examples/with-lingui/stories/index.stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+import styled from '@emotion/styled/macro'
+const Red = styled.div`
+background-color: red;
+`
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <Red role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </Red>
+    </Button>
+  ));
